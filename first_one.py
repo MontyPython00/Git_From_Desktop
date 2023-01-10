23848363679baa90e7da7392a1cdea9c436c0cfc
MontyPython00 <montiback22@gmail.com>
2023-01-10 18:37:21 +0100

    another error

--- first_one.py
+++ first_one.py
@@ -1,5 +1,19 @@
-print('hello world')
-for i in range(10):
-    print(i)
-    print('test')
+import time
+import re
+li = []
+test = '123 nothing'
+
+# while test != 'stop':
+#     print('To stop a software insert \'stop\'')
+#     test = input('test:')
+#
+#     time.sleep(1)
+#     li.append(test)
+#     print(li)
+
+li.append(re.findall("(\d+)?(\D+)?(\d+)", test))
+testing = re.findall("(\d+)?(\D+)?(\d+)", test)
+print(li, testing[0][1])
+
+
 
