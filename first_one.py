12f5c89e5613a3042a88f3fcbe062549ae56b88f
MontyPython00 <montiback22@gmail.com>
2023-01-10 18:24:41 +0100

    software to divide numbers from letters

--- first_one.py
+++ first_one.py
@@ -1,4 +1,17 @@
-print('hello world')
-for i in range(1, 10):
-  print(i)
-  
+import time
+import re
+li = []
+test = '123 nothing'
+
+# while test != 'stop':
+#     print('To stop a software insert \'stop\'')
+#     test = input('test:')
+#
+#     time.sleep(1)
+#     li.append(test)
+#     print(li)
+
+li.append(re.findall("(\d+)?(\D+)?(\d+)", test))
+testing = re.findall("(\d+)?(\D+)?(\d+)", test)
+print(li, testing[0][1])
+
